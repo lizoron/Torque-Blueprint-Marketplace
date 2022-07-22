23fa176d8dedd0b7de75e748a4de9f3fd63730b4
ophir.h <ophir.h@quali.com>
2022-07-22 16:08:28 +0300

    refactor user_data

--- terraform-modules/spec1-sample/instance.tf
+++ terraform-modules/spec1-sample/instance.tf
@@ -9,26 +9,5 @@ network_interface {
 }
 
   tags = {"Key": "Name","Value": "java-spring-website"}
-  user_data = base64encode(join("",["",
-                                "#!/bin/sh\n",
-                                "apt-get update -y\n",
-                                "apt-get install default-jdk -y\n",
-                                "apt-get install tomcat8 -y\n",
-                                "apt-get install tomcat8 -y\n",
-                                "apt-get install tomcat8-admin -y\n",
-                                "mkdir /home/artifacts\n",
-                                "cd /home/artifacts || exit\n",
-                                "git clone https://github.com/cloudshell-colony/sample_java_spring_source.git\n",
-                                "mkdir /home/user/.config/colony-java-spring-sample -p\n",
-                                "bash -c 'cat >> /home/user/.config/colony-java-spring-sample/app.properties' <<EOL\n",
-                                "# Dadabase connection settings:\n",
-                                "jdbc.url=jdbc:mysql://mysql.$DOMAIN_NAME:3306/$DB_NAME\n",
-                                "jdbc.username=$DB_USER\n",
-                                "jdbc.password=$DB_PASS\n",
-                                "EOL\n",
-                                "echo 'Deploy to TomCat'\n",
-                                "rm -rf /var/lib/tomcat8/webapps/ROOT\n",
-                                "cp sample_java_spring_source/artifacts/colony-java-spring-sample-1.0.0-BUILD-SNAPSHOT.war /var/lib/tomcat8/webapps/ROOT.war\n",
-                                "systemctl start tomcat8\n"
-                            ]))
+	user_data = tostring(file("java-spring-website.sh"))
 }
