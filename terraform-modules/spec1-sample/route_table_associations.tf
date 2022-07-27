166f147636ad6f71111057602d94a7fca2bbcff5
ophir.h <ophir.h@quali.com>
2022-07-27 20:54:05 +0300

    remove mng association

--- terraform-modules/spec1-sample/route_table_associations.tf
+++ terraform-modules/spec1-sample/route_table_associations.tf
@@ -8,7 +8,7 @@ resource "aws_route_table_association" "AppSubnet1RouteAssociation" {
   subnet_id = aws_subnet.AppSubnet1.id
 }
 
-resource "aws_route_table_association" "MngSubnetRouteAssociation" {
+/*resource "aws_route_table_association" "MngSubnetRouteAssociation" {
   route_table_id = aws_route_table.PublicRouteTable.id
   subnet_id = aws_subnet.MngSubnet.id
-}
+}*/
