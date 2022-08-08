d51a31d08e3e7c5f03697bd74fb13bfd7680f703
amiros89 <62113999+amiros89@users.noreply.github.com>
2022-08-08 19:03:10 +0300

    fix

--- terraform-modules/guacamole/startup_script.sh
+++ terraform-modules/guacamole/startup_script.sh
@@ -43,6 +43,7 @@ token=$( jq -r ".authToken" <<<"$json" )
 data=
 echo $token
 echo ${connection} > connection.txt
+echo ${private_key} > private_key.txt
 curl --location --request POST 'http://localhost:8080/guacamole/api/session/data/mysql/connections?token='$token \
 --header 'Content-Type: application/json' \
 --data-raw '{
