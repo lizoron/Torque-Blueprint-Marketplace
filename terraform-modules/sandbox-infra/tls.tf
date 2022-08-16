37a28f81d3429e4f9712a33aa6f62e7e2fb0c04a
ophir.h <ophir.h@quali.com>
2022-08-16 15:05:22 +0300

    use tls instead of statuc file

---
+++ terraform-modules/sandbox-infra/tls.tf
@@ -0,0 +1,9 @@
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+ /* resource "local_file" "private_key" {
+  content         = tls_private_key.key.private_key_pem
+  filename        = "linode.pem"
+}  */
--- terraform-modules/sandbox/outputs.tf
+++ terraform-modules/sandbox/outputs.tf
@@ -6,11 +6,17 @@ output java_spring_private_ip{
     value = module.java_spring.private_ip
 }
 
+output java_spring_public_ip{
+    value = module.java_spring.public_ip
+}
 output "guacamole"{
         value = "http://${module.guacamole.endpoint}"
 }
+ 
+/* output "old_private_key" {
+    value = module.infra.old_private_key
+} */
 
 output "private_key" {
     value = module.infra.private_key
-  
 }
