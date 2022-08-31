0037b3f1f0de197669eeeb968b69dc6cef5ca714
ophir.h <ophir.h@quali.com>
2022-08-31 12:00:09 +0300

    succeed get oidc of the cluster

--- terraform-modules/eks-cluster/oidc.tf
+++ terraform-modules/eks-cluster/oidc.tf
@@ -1,15 +1,16 @@
-resource "null_resource" "iam_oidc_provider" {
+ resource "null_resource" "iam_oidc_provider" {
   provisioner "local-exec" {
     command = "eksctl utils associate-iam-oidc-provider --region=${var.aws_region} --cluster=${data.aws_eks_cluster.eks.name} --approve"
   }
-}
+} 
 
-data "external" "oidc" {
-  program = ["aws eks describe-cluster --name ${data.aws_eks_cluster.eks.name} --query 'cluster.identity.oidc.issuer' --output text"]
+data "external" "test" {
+  program = ["python", "${path.module}/scripts/get_oidc.py",data.aws_eks_cluster.eks.name]
 
+  
 }
 
 output "oidc" {
-    value = data.external.oidc.result
+    value = data.external.test.result["oidc"]
   
 }
