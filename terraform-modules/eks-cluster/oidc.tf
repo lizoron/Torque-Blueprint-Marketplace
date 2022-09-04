8cf0b3b017d9a1675cc6dcb83c4b0879f000b454
ophir.h <ophir.h@quali.com>
2022-09-04 14:09:03 +0300

    try install curl using apt-get

--- terraform-modules/eks-cluster/oidc.tf
+++ terraform-modules/eks-cluster/oidc.tf
@@ -1,5 +1,5 @@
 locals {
-  install_eksctl_cmd = "curl --silent --location 'https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz' | tar xz -C /tmp; mv /tmp/eksctl /usr/local/bin"
+  install_eksctl_cmd = "apt-get update -y; apt-get install -y curl; curl --silent --location 'https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz' | tar xz -C /tmp; mv /tmp/eksctl /usr/local/bin"
   create_oidc_cmd = "eksctl utils associate-iam-oidc-provider --region=${var.aws_region} --cluster=${data.aws_eks_cluster.eks.name} --approve;"
   cmd = var.install_eksctl == "yes" ? join(";",[local.install_eksctl_cmd,local.create_oidc_cmd]): local.create_oidc_cmd
 } 
