29af34d749fd0303a20ebb3a3dccf6df12b3e374
ophir.h <ophir.h@quali.com>
2022-09-04 21:36:47 +0300

    update permissions of eks-cluster using oidc

--- terraform-modules/eks-cluster/oidc.tf
+++ terraform-modules/eks-cluster/oidc.tf
@@ -1,6 +1,6 @@
 locals {
   install_eksctl_cmd = "apt-get update -y; apt-get install -y curl; curl --silent --location 'https://github.com/weaveworks/eksctl/releases/download/v0.110.0/eksctl_Linux_amd64.tar.gz' | tar xz -C /tmp; mv /tmp/eksctl /usr/local/bin"
-  create_oidc_cmd = "eksctl utils associate-iam-oidc-provider --region=${var.aws_region} --cluster=${data.aws_eks_cluster.eks.name} --approve;"
+  create_oidc_cmd = "eksctl utils associate-iam-oidc-provider --region ${var.aws_region} --cluster ${data.aws_eks_cluster.eks.name} --approve"
   cmd = var.install_eksctl == "yes" ? join(";",[local.install_eksctl_cmd,local.create_oidc_cmd]): local.create_oidc_cmd
 } 
  
