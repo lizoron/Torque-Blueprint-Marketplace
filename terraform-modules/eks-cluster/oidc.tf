badaed19ceff173aebda680073a201f01a81c5ec
ophir.h <ophir.h@quali.com>
2022-09-04 13:14:38 +0300

    fix concating string

--- terraform-modules/eks-cluster/oidc.tf
+++ terraform-modules/eks-cluster/oidc.tf
@@ -1,7 +1,7 @@
 locals {
-  install_eksctl_cmd = "curl --silent --location 'https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz' | tar xz -C /tmp; sudo mv /tmp/eksctl /usr/local/bin;"
+  install_eksctl_cmd = "curl --silent --location 'https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz' | tar xz -C /tmp; sudo mv /tmp/eksctl /usr/local/bin"
   create_oidc_cmd = "eksctl utils associate-iam-oidc-provider --region=${var.aws_region} --cluster=${data.aws_eks_cluster.eks.name} --approve;"
-  cmd = var.install_eksctl == "yes" ? local.install_eksctl_cmd + local.create_oidc_cmd : local.create_oidc_cmd
+  cmd = var.install_eksctl == "yes" ? join(";",[local.install_eksctl_cmd,local.create_oidc_cmd]): local.create_oidc_cmd
 } 
  
  resource "null_resource" "iam_oidc_provider" {
