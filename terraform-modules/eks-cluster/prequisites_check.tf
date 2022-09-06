4df80b222da98edb21c9c3552a7702b5e6280401
ophir.h <ophir.h@quali.com>
2022-09-06 09:01:36 +0300

    add aws cli prequisite check

--- terraform-modules/eks-cluster/prequisites_check.tf
+++ terraform-modules/eks-cluster/prequisites_check.tf
@@ -2,12 +2,13 @@ locals{
     python_check_cmd = "python --version"
     eksctl_check_cmd = "eksctl version"
     kubectl_check_cmd = "kubectl version --short"
+    awscli_check_cmd = "aws --version"
 }
 
 
 resource null_resource "prequisites_check"{
     provisioner "local-exec" {
         when = create
-        command = join(" ; ",[local.python_check_cmd,local.eksctl_check_cmd,local.kubectl_check_cmd])
+        command = join(" ; ",[local.python_check_cmd,local.eksctl_check_cmd,local.kubectl_check_cmd,local.awscli_check_cmd])
     }
 }
