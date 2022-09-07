58f353a02ae8b9a8076e029c67eb3ab58f637185
Ophir Haramaty <dreckguy@gmail.com>
2022-09-07 10:18:23 +0300

    Update local_connection.tf

--- terraform-modules/eks-cluster/local_connection.tf
+++ terraform-modules/eks-cluster/local_connection.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "local_connection" {
-  count = var.local_connection =="yes" ? 1 : 0
+  count = var.connect_to_cluster =="yes" ? 1 : 0
   provisioner "local-exec" {
     command = "aws --region ${var.aws_region} eks update-kubeconfig --name ${data.aws_eks_cluster.eks.name}"
   }
