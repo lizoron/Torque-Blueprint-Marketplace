591913a24bd861d17afc24534df45638194440a8
lizoron <ericsilver20@gmail.com>
2023-05-02 18:14:47 -0500

    test

--- terraform-modules/s3-bucket-website/config.tf
+++ terraform-modules/s3-bucket-website/config.tf
@@ -11,3 +11,20 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+resource "aws_s3_bucket_public_access_block" "acc_bl" {
+  bucket = aws_s3_bucket.files.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "owner_cont" {
+  bucket = aws_s3_bucket.files.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
