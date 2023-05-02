f0b57d5c3733bf7064c72beaf6a018ea6515c52a
lizoron <ericsilver20@gmail.com>
2023-05-02 18:01:05 -0500

    test6

--- terraform-modules/s3-bucket-website/website-config.tf
+++ terraform-modules/s3-bucket-website/website-config.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket_website_configuration" "default" {
     depends_on = [
-        aws_s3_bucket_ownership_controls.owner_cont,
-        aws_s3_bucket_public_access_block.acc_bl,
+        aws_s3_bucket_ownership_controls.owner_cont
+        #aws_s3_bucket_public_access_block.acc_bl,
     ]
   bucket = aws_s3_bucket.files.id
   index_document {
