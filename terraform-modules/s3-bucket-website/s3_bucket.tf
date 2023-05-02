1f0d581288fa9e59a4300552ffbfd2a526f7cddc
lizoron <ericsilver20@gmail.com>
2023-05-02 18:23:13 -0500

    test

--- terraform-modules/s3-bucket-website/s3_bucket.tf
+++ terraform-modules/s3-bucket-website/s3_bucket.tf
@@ -3,5 +3,4 @@ resource "aws_s3_bucket" "files" {
         aws_s3_bucket_ownership_controls.owner_cont
         #aws_s3_bucket_public_access_block.acc_bl,
     ]
-    bucket = aws_s3_bucket.files.id
 }
