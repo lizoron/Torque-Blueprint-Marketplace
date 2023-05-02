7740ce56ca69c06d0e8353bcf2d150556d3847df
lizoron <ericsilver20@gmail.com>
2023-05-02 17:38:57 -0500

    test

--- terraform-modules/s3-bucket-website/index_object.tf
+++ terraform-modules/s3-bucket-website/index_object.tf
@@ -3,10 +3,31 @@ locals{
   }
 
 resource "aws_s3_object" "index" {
+  depends_on = [
+        aws_s3_bucket_ownership_controls.owner_cont,
+        aws_s3_bucket_public_access_block.acc_bl,
+  ]
   bucket = aws_s3_bucket.files.id
   key    = "index.html"
   source = local.index_path
   etag = filemd5(local.index_path)
   content_type = "text/html"
   acl = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "acc_bl" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "owner_cont" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
