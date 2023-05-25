97c48d1aa6c8b62fcd64cb54b505363df59704d8
lizoron <ericsilver20@gmail.com>
2023-05-25 10:08:47 -0500

    yesy

--- terraform-modules/basic-vm/main.tf
+++ terraform-modules/basic-vm/main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "myec2" {
     ami = "ami-0ab193018f3e9351b"
     instance_type = "t2.micro"
     tags = {
-        Name = "erics-test-instance-t2micro"
+        Name = "erics-test-instance"
     }
 }
